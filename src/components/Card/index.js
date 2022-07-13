eb7de589ed87b6209304673fe89a7441ff598c83
tnmrs <promomyers@gmail.com>
2022-07-13 22:50:49 +0300

    favorites(liked)

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,31 +1,32 @@
 import styles from './Card.module.scss';
 import React from 'react';
 
-function Card({ onClickFavorite, imageUrl, title, price, onClickPlus }) {
+function Card({ onAddLiked, imageUrl, title, price, onClickPlus }) {
   const [isAdded, setIsAdded] = React.useState(false);
 
-  // const [isLike, setIsLike] = React.useState(false);
+  const [isLike, setIsLike] = React.useState(false);
 
   const handle = () => {
     onClickPlus({ title, imageUrl, price });
     setIsAdded(!isAdded);
   };
 
-  // const select = () => {
-  //   setIsLike(!isLike);
-  // };
+  const onClickLiked = () => {
+    onAddLiked({ title, imageUrl, price });
+    setIsLike(!isLike);
+  };
 
   return (
     <div className={styles.card}>
-      <div className={styles.favorite} onClick={onClickFavorite}>
-        <img src="/img/heart-unliked.svg" alt="Unliked" />
+      <div className={styles.favorite} onClick={onClickLiked}>
+        <img src={isLike ? '/img/heart-liked.svg' : '/img/heart-unliked.svg'} alt="Unliked" />
       </div>
       <img width={133} height={112} src={imageUrl} alt="Sneakers" />
       <h5>{title}</h5>
       <div className="d-flex justify-between align-center">
         <div className="d-flex flex-column">
           <span>Цена:</span>
-          <b>{price}</b>
+          <b>{price} руб.</b>
         </div>
         <img
           className={styles.plus}
